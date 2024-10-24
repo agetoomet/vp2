2a07403a4259a76a3352546dd1b1a9e663980d60
Age Toomet <agett17@users.noreply.github.com>
2024-10-24 10:35:49 +0300

    Lisatud andmebaasiühenuds

---
+++ dateTime.js
@@ -0,0 +1,121 @@
+const dayNamesEt = ["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev"];
+const monthNamesEt = ["jaanuar", "veebruar","märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detseber"]; 
+const dateFormatted = function(){	
+	//function dateFormatted(){
+	let timeNow = new Date();
+	let dateNow = timeNow.getDate();
+	let monthNow = timeNow.getMonth();
+	let yearNow = timeNow.getFullYear();
+	
+	//console.log("Täna on: " + dateNow + "." + (monthNow + 1) + "." + yearNow);
+	//let dateEt = dateNow + ". " + monthNamesEt[monthNow] + " " + yearNow;
+	//return dateEt;
+	return dateNow + ". " + monthNamesEt[monthNow] + " " + yearNow;
+} 
+
+//const weekDayEt = function(){
+	//let timeNow = new Date();
+	//let dayNow = timeNow.getDay();
+	//return dayNamesEt[dayNow];
+//}
+
+const dayFormatted = function(){
+	let timeNow = new Date();
+	let dayNow = timeNow.getDay();
+	let dayEt = dayNamesEt[dayNow];
+	return dayEt;
+}
+
+const timeFormatted = function(){
+	let timeNow = new Date();
+	let hourNow = timeNow.getHours();
+	let minutesNow = timeNow.getMinutes();
+	let secondsNow = timeNow.getSeconds();
+	let timeEt = hourNow + ":" + minutesNow + ":" + secondsNow;
+	return timeEt;
+}
+
+const partOfDay = function (){
+	let dPart = "lihtsalt aeg";
+	let dayNow = new Date ().getDay();
+	let dayNameNow = dayNamesEt[dayNow]
+	let hourNow = new Date ().getHours();
+	// OR || AND &&
+	// >  <    >=   <=  !=   ==    ===
+	if (dayNameNow === "pühapäev" || dayNameNow === "laupäev"){
+		console.log("Täna on nädalavahetus: " + dayNameNow);
+				if (dayNameNow === "pühapäev"){
+			if ( hourNow < 9){
+				dPart = "uneaeg";
+			} else if (hourNow > 10 && hourNow < 13){
+				dPart = "õppimise aeg";
+			} else if (hourNow >= 13 && hourNow <= 19){
+				dPart = "töö";
+			} else {
+				dPart = "vaba aeg";
+			}
+		} else if (dayNameNow === "laupäev"){
+			if (hourNow >= 10 && hourNow < 19){
+				dPart = "õppimise aeg"; 
+			} else {
+				dPart = "vaba aeg";
+			}
+		}
+	} else if (dayNameNow === "esmaspäev" || dayNameNow === "teisipäev" || dayNameNow === "kolmapäev" || dayNameNow === "neljapäev" || dayNameNow === "reede"){ 
+	console.log("Täna on tööpäev: " + dayNameNow);
+		if(dayNameNow === "esmaspäev"){
+			if (hourNow < 8 ){
+				dPart = "uneaeg";
+			} else if (hourNow > 10 && hourNow <= 20){
+				dPart = "kooliaeg";
+			} else {
+				dPart = "vaba aeg";
+			}
+		} else if (dayNameNow === "teisipäev") {
+            if (hourNow < 8) {
+                dPart = "uneaeg";
+			} else if (hourNow > 10 && hourNow < 14){
+				dPart = "kooliaeg";
+			} else if (hourNow > 14 && hourNow < 16){
+				dPart = "trenn";
+			} else if (hourNow > 16 && hourNow <= 19){
+				dPart = "õppimise aeg";
+			} else {
+				dPart = "vaba aeg";
+			}
+		} else if (dayNameNow === "kolmapäev"){
+			if (hourNow > 8 && hourNow < 14){
+				dPart = "kooliaeg";
+			} else if (hourNow > 15 && hourNow < 18){
+				dPart = "õppimise aeg";
+			} else if (hourNow > 18 && hourNow < 20){
+				dPart = "trenn";
+			} else {
+				dPart = "vaba aeg";
+			}
+		} else if (dayNameNow === "neljapäev"){
+			if (hourNow > 10 && hourNow < 12){
+				dPart = "kooliaeg"
+			} else if (hourNow > 12 && hourNow < 18){
+				dPart = "õppimiseaeg";
+			} else {
+				dPart = "vaba aeg";
+			}
+		} else if (dayNameNow === "reede"){
+			if (hourNow > 10 && hourNow < 12){
+				dPart = "trenn";
+			} else if (hourNow > 13 && hourNow < 18){
+				dPart = "õppimise aeg";
+			} else {
+				dPart = "vaba aeg";
+			}
+		}
+	} else {
+		dPart = "vaba aeg";
+	}
+	
+	return dPart;
+}
+
+//ekspordin kõik vajaliku
+module.exports = {dateFormatted: dateFormatted, dayFormatted: dayFormatted, timeFormatted: timeFormatted, dayNames: dayNamesEt, monthNames: monthNamesEt, dayPart: partOfDay};
--- index.js
+++ index.js
@@ -1,8 +1,173 @@
 const express = require("express");
+const dateTime = require("./dateTime");
+const fs = require("fs");
+//et saada kõik pärignust kätte
+const bodyparser = require("body-parser");
+//andmebaasi andmed
+const dbInfo = require("../../vp2024config");
+//andmebaasiga suhtlemine
+const mysql = require("mysql2");
+
 const app = express();
 
-app.get("/", (req, res)=>{
-	res.send("Express läks käima!");
+//määran viev mootori
+app.set("view engine", "ejs");
+//määran jagatavate, avalike failide kausta
+app.use(express.static("public"));
+//kasutame body-parserit päringute parsimiseks (kui ainult tekst, siis false, kui ka pildid jms, siis true)
+app.use(bodyparser.urlencoded({extended: false}));
+
+//loon andmebaasi ühenduse
+const conn = mysql.createConnection({
+	host: dbInfo.configData.host,
+	user: dbInfo.configData.user,
+	password: dbInfo.configData.passWord,
+	database: dbInfo.configData.dataBase
 });
+
+app.get("/", (req, res)=>{
+	//res.send("Express läks käima!");
+	//console.log(dbInfo.configData.host);
+	res.render("index");
+});
+
+app.get("/timenow", (req, res)=>{
+	const weekdayNow = dateTime.dayFormatted();
+	const dateNow = dateTime.dateFormatted();
+	const timeNow = dateTime.timeFormatted();
+	res.render("timenow", {nowWD: weekdayNow, nowD: dateNow, nowT: timeNow});
+});
+
+app.get("/vanasonad", (req, res) => {
+    let folkWisdom = [];
+    fs.readFile("public/textfiles/vanasonad.txt", "utf8", (err, data) => {
+        if (err) {
+            res.render("justlist", { h2: "Vanasõnad", listData: ["Ei leidnud midagi!"] });
+        } else {
+            folkWisdom = data.split(";");
+            res.render("justlist", { h2: "Vanasõnad", listData: folkWisdom });
+        }
+    });
+});
+
+app.get("/regvisit", (req, res)=>{
+	const weekdayNow = dateTime.dayFormatted();
+    const dateNow = dateTime.dateFormatted();
+    const timeNow = dateTime.timeFormatted();
+
+	res.render("regvisit", {
+		nowWD: weekdayNow,
+		nowD: dateNow,
+		nowT: timeNow
+	});
+});
+
+app.post("/regvisit", (req, res)=>{
+	//console.log(req.body);
+	const weekdayNow = dateTime.dayFormatted();
+    const dateNow = dateTime.dateFormatted();
+    const timeNow = dateTime.timeFormatted();
+
+    // Logime kasutaja sisestatud andmed koos kuupäeva ja kellaajaga
+    const logEntry = `${req.body.firstNameInput} ${req.body.lastNameInput}; ${weekdayNow}, ${dateNow}, ${timeNow}\n`;
+	//avan txt faili selliselt, et kui seda pole olemas luuakse
+	fs.open("public/textfiles/log.txt", "a", (err, file)=>{
+		if(err){
+			throw err;
+		}
+		else {
+			fs.appendFile("public/textfiles/log.txt", logEntry, (err)=>{
+				if(err){
+					throw err;
+				}
+				else {
+					console.log("Faili kirjutati!");
+					
+					res.render("regvisit", {
+						nowWD: weekdayNow,
+						nowD: dateNow,
+						nowT: timeNow
+					});
+				}
+			});
+		}
+	});
+	//res.render("regvisit");
+});
+
+app.get("/visitlog", (req, res) => {
+    fs.readFile("public/textfiles/log.txt", "utf8", (err, data) => {
+        if (err) {
+            return res.render("justlist", { h2: "Registreeritud külastajad", listData: ["Ei leidnud logifaili!"] });
+        }
+        
+        const visits = data.trim().split("\n").filter(entry => entry.length > 0);
+		console.log("Külastuste logi:", visits); // Logige külastuste andmed konsooli
+        res.render("justlist", { h2: "Registreeritud külastajad", listData: visits });
+    });
+});
+
+app.get("/regvisitdb", (req, res)=>{
+	let notice = "";
+	let firstName = "";
+	let lastName = "";
+	res.render("regvisitdb", {notice: notice, firstName: firstName, lastName: lastName});
+});
+
+app.post("/regvisitdb", (req, res)=>{
+	let notice = "";
+	let firstName = "";
+	let lastName = "";
+	//kontrollin kas kõik vajalikud andmed on olemas
+	if(!req.body.firstNameInput || !req.body.lastNameInput){
+		//console.log("Osa andmeid puudu");
+		notice = "Osa andmeid puudu";
+		firstName = req.body.firstNameInput;
+		lastName = req.body.lastNameInput;
+		res.render("regvisitdb", {notice: notice, firstName: firstName, lastName: lastName});
+	}
+	else {
+		let sqlReq = "INSERT INTO vp2visitlog (first_name, last_name) VALUES (?,?)";
+		conn.query(sqlReq, [req.body.firstNameInput, req.body.lastNameInput], (err, sqlRes)=>{
+			if(err){
+				notice = "Tehnilistel põhjustel andmeid ei salvestatud!";
+				res.render("regvisitdb", {notice: notice, firstName: firstName, lastName: lastName});
+				throw err;
+			}
+			else{
+				//notice = "Andmeid salvestati";
+				//res.render("regvisitdb", {notice: notice, firstName: firstName, lastName: lastName});
+				res.redirect("/");
+			}
+		});
+	}
+});	
+
+app.get("/eestifilm", (req, res)=>{
+	res.render("eestifilm");
+});
+
+app.get("/eestifilm/tegelased", (req, res)=>{
+	//loon andmebaasi päringu
+	let sqlReq = "SELECT first_name, last_name, birth_date FROM person";
+	conn.query(sqlReq, (err, sqlRes)=>{
+		if (err){
+			res.render("tegelased",{persons: []});
+			//throw err;
+		}
+		else{
+			//console.log(sqlRes);
+			res.render("tegelased",{persons: sqlRes});
+		}
+	});
+	//res.render("tegelased");
+});
+
+app.get("/eestifilm/lisa", (req, res)=>{
+	res.render("addperson");
+});
+
+
+
 
 app.listen(5212);
